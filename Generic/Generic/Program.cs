92c10ffb1efc1009bbd896d0b200dc5791146686
pavankalyan1611 <pkkoppisetti@gmail.com>
2022-06-17 15:57:25 +0530

    [pavan] Welcome to UC1_Without_Generic

---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Generic
+{
+    class Program
+    {
+        public static void Main(string[] arg)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9 };
+            char[] charArray = { 'p', 'a', 'v', 'a', 'n' };
+
+            UC1_WithoutGeneric.toPrint(intArray);
+            UC1_WithoutGeneric.toPrint(doubleArray);
+            UC1_WithoutGeneric.toPrint(charArray);
+
+
+        }
+    }
+}
