e6c0d747161528e1df5f3030a6b179fe5e99f829
pavankalyan1611 <105772560+pavankalyan1611@users.noreply.github.com>
2022-06-17 16:06:17 +0530

    Update Program.cs

--- Generic/Generic/Program.cs
+++ Generic/Generic/Program.cs
@@ -6,6 +6,8 @@ namespace Generic
     {
         public static void Main(string[] arg)
         {
+            Console.WriteLine("Welcome to UC1 of  an array of Integer,Double and Character Print the same without Generic : ");
+            
             int[] intArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9 };
             char[] charArray = { 'p', 'a', 'v', 'a', 'n' };
